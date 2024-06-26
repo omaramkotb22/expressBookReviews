d076cd22b7d31cc7561dcfa8a44f4057ffc08b42
omaramkotb22 <95895935+omaramkotb22@users.noreply.github.com>
2024-06-27 03:54:08 +0400

    chore: Add book review functionality and authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,20 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, "secret_key");
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
 });
  
 const PORT =5000;
